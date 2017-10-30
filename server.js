d5fc3cf6e6716c4998aaf55ccbc69efe270017a8
supun19 <supun.12@cse.mrt.ac.lk>
2017-10-30 12:02:16 +0530

    directpay

--- server.js
+++ server.js
@@ -1,29 +1,10 @@
-var express = require('express');
-app = express();
-var bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.get('/', function (req, res) {
+    res.sendfile('index.html')
+})
 
-port = process.env.PORT || 3000;
-
-
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-
-app.listen(port);
-
-console.log("Server is listning to ....."+port);
+app.listen(3001, function () {
+    console.log('Example app listening on port 3001!')
+})
